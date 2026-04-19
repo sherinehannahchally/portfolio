c3d93c554986acf298329cc378f9452efe51a9fd
Sherine Chally <sherinechally@gmail.com>
2026-04-19 02:33:05 -0700

    refactor app configuration by removing unused animation provider to streamline the setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations'; // Add this line
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideAnimations() // Add this here
-  ]
+  providers: [provideRouter(routes)],
 };
