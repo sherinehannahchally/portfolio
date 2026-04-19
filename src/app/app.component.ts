d85151a2e0b99e95fb018aebabc4a28ea6f62539
Sherine Chally <sherinechally@gmail.com>
2026-04-19 02:32:05 -0700

    remove unused imports from app.component.ts to simplify the component structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
